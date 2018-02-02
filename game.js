5fd4d2cbb6af558b9d958f66f18e7aea7cf80026
Russ Carr <russ.mdr@gmail.com>
2018-02-02 14:19:25 -0700

    first part

---
+++ game.js
@@ -0,0 +1,99 @@
+var health = 100
+var slapCount = 0
+var kickCount = 0
+var punchCount = 0
+var hitCount = 0
+
+
+function hitCounter() {
+    hitCount = (slapCount + punchCount + kickCount)
+    document.getElementById("hitCounter").innerHTML = hitCount;
+    update()
+
+}
+
+function slap(){
+    health = (health - 1);
+    slapCount++
+    hitCounter()
+    update()
+
+}
+
+function punch(){
+    health = (health - 5);
+    punchCount++
+    hitCounter()    
+    update()
+
+}
+
+function kick(){
+    health = (health - 10);
+    kickCount++
+    hitCounter()
+    update()
+
+}
+function reset(){
+    health = 100
+    hitCount = 0
+    update()
+
+}
+
+function update(){
+       document.getElementById("healthDisplay").innerHTML = health;
+       document.getElementById("hitCounter").innerHTML = hitCount;
+}
+
+/*Jame Parrott  
+Stephania Proctor  
+Maritza Stoddart  
+Collette Molino  
+Antoine Penaloza  
+Adela Michelsen  
+Pok Herrell  
+Sonny Devore  
+Suzan Weatherwax  
+Tobie Deville  
+Humberto Bregman  
+Chastity Gathright  
+Thora Dubay  
+Lory Kirschner  
+Melonie Room  
+Dina Binder  
+Kimberley Bettinger  
+Marylou Whipple  
+Amy Halls  
+Dylan Show  
+Delana Dudgeon  
+Heidi Bugbee  
+Elvera Noble  
+Rolanda Zuniga  
+Petronila Brooker  
+Charmaine Brockway  
+Starr Sproull  
+Yee Mehler  
+Violette Clover  
+Clifton Novick  
+Dominic Pettigrew  
+Isabel Aceto  
+Otilia Vanhoose  
+Valentin Kervin  
+Gabriel Kosinski  
+Christene Knipe  
+Rosaline Tabor  
+Laureen Both  
+Lavera Lenhardt  
+Bryant Mukai  
+Percy Petzold  
+Jama Finnen  
+Janessa Derossett  
+Joselyn Short  
+Lorretta Stefanski  
+Elenora Leeper  
+Delfina Redick  
+Hulda Rowton  
+Lashaun Ryberg  
+Helga Tackitt  */
